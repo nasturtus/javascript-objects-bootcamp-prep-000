6ac0e16901008e61be4ac79b28e90cea55fc13d3
Santosh SN <nasturtus@gmail.com>
2018-02-03 06:49:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   mercedes_sosa: 'todo cambio',
   eugenia_leon: 'el presente'
 };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist
+}
